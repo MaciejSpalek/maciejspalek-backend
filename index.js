a3a8fc4749fdea5aec144088858d58b5bdab753d
Maciej Spałek <maciej.spalek@proexe.pl>
2024-01-09 18:29:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
+const cors = require("cors");
+
+dotenv.config({path: __dirname + "/.env"});
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//   mongoose.connect(
+//     process.env.DB_CONNECT,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     () => console.log("Connected to database")
+//   );
+
+app.listen(PORT, () => console.log(`Server is running...`));
