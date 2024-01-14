c08052c432841f2a8c5cc7151df89a25622fa0a2
Maciej Spałek <maciej.spalek@proexe.pl>
2024-01-14 20:47:10 +0100

    Changed index.js to app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 const cors = require("cors");
-// const craftRoute = require("./src/routes/craft");
 const aboutSectionRoute = require("./src/routes/aboutSection");
 const mongoose = require("mongoose");
 
@@ -28,22 +27,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use("/api/craft", craftRoute);
 app.use("/api/about-section", aboutSectionRoute);
-
-// mongoose
-//   .createConnection(process.env.MONGO_DB_URL, {
-//     auth: {
-//       username: process.env.MONGO_DB_USER,
-//       password: process.env.MONGO_DB_PASSWORD,
-//     },
-//   })
-//   .then(() => {
-//     console.log("Connected to db");
-//   })
-//   .catch((error) => {
-//     console.log("Something went wrong", error);
-//   });
 
 mongoose
   .connect(process.env.MONGO_DB_URL, {
@@ -57,13 +41,5 @@ mongoose
   .catch((error) => {
     console.log("Something went wrong", error);
   });
-
-// mongoose.createConnection("mongodb://localhost:27017/dbName", {
-//   auth: {
-//     authSource: "admin",
-//   },
-//   user: "admin",
-//   pass: "password",
-// });
 
 app.listen(PORT, () => console.log(`Server is running...`));
