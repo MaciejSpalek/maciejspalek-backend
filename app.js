d144c5585f157cb37231ec658ecc3e64c7c03068
Maciej Spałek <maciej.spalek@proexe.pl>
2024-05-21 20:33:23 +0200

    Added token on server running

--- app.js
+++ app.js
@@ -43,4 +43,4 @@ app.use("/images", express.static("images"));
 connection();
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server is running on port ${port}...`));
+app.listen(port, console.log(`Server is running on port ${port}..., ${process.env.TOKEN_SECRET}`));
