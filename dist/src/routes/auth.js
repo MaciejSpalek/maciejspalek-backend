bca642517fa9fa813d753d5d9fabbb04f121d6b7
MaciejSpalek <spwrtt@gmail.com>
2026-02-19 14:56:27 +0100

    feat(typescript): Fix auth

--- dist/src/routes/auth.js
+++ dist/src/routes/auth.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { authController } from "../controllers";
+import verifyToken from "../middleware/verifyToken";
 const router = Router();
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.get("/verifyUser", verifyToken, authController.verifyUser);
 export default router;
